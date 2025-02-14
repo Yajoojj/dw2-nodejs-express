61e61a08fa00ddc9701e3c12697661da9ce29032
Yago Uran Kurashiki Rios <145205598+Yajoojj@users.noreply.github.com>
2025-02-13 21:46:42 -0300

    Add files via upload

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,20 @@
+// Declaração de variavel no JS
+// CONST x LET x VAR
+// Por questão de segurança não usar VAR
+// No escopo global, "CONST" não pode ser alterado
+
+const nome = "Yago" // Precisa de Valor inicial
+let Nome // Permite iniciar variavel vazia
+
+// const nome = "Pedro" -> Resulta um erro
+// let Nome -> Resulta em um erro
+
+var cor = "Azul"
+var cor = "Amarelo"
+
+let cidade = "Registro"
+
+cidade = "Cananéia" // let permite alterar o valor.
+
+const message = "Hello, World!"
+console.log(message)
