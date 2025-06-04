5cf365a0a73aff8c653ee6e411157bcdc411c108
Yajoojj <yaokurashiki@outlook.com>
2025-06-04 20:58:30 -0300

    Yajoojj

---
+++ Aulas/node-JS-aula-07-versao-pratica/index.js
@@ -0,0 +1,31 @@
+﻿import express from "express";
+import MoviesController from "./controllers/MoviesController.js";
+import connection from "./config/sequelize-config.js";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false}));
+app.use("/", MoviesController);
+
+
+connection.authenticate().then(() => {
+    console.log("Conexão com o banco de dados feita com sucesso!");
+}).catch((error) => {
+    console.log(error);
+});
+
+connection.query(`CREATE DATABASE IF NOT EXISTS movies;`).then(() => {
+    console.log("O banco de dados está criado.");
+}).catch((error) => {
+    console.log(error);
+});
+
+const port = 8080;
+app.listen(port, (error) => {
+    if (error) {
+        console.log(`Ocorreu um erro: ${error}`);
+    } else {
+        console.log(`Servidor iniciado em http://localhost:${port}`);
+    }
+});
---
+++ Aulas/node-JS-aula-07-versao-pratica/models/Movie.js
@@ -0,0 +1,19 @@
+﻿import Sequelize from "sequelize";
+import connection from "../config/sequelize-config.js";
+
+const Movie = connection.define("movies", {
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    genre:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    duration:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+});
+Movie.sync({ force: false});
+export default Movie;
